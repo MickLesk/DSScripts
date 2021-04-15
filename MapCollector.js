f43fb7870eeb1f505d2e5d007e398fae7b9b53ff
MickLesk <mickey.leskowitz@gmail.com>
2021-04-15 14:01:55 +0200

    Beautify code

--- MapCollector.js
+++ MapCollector.js
@@ -9,7 +9,7 @@ The above copyright notice and this permission notice shall be included in all c
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRE\u00DF OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNE\u00DF FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-ScriptAPI.register( 'MapCollector', true, '-Energy Shots', 'support-nur-im-forum@die-staemme.de' );
+ScriptAPI.register('MapCollector', true, '-Energy Shots', 'support-nur-im-forum@die-staemme.de');
 
 var mapOverlay;
 var coords = [];
@@ -26,35 +26,35 @@ function fnRefresh() {
 }
 
 function closeCoordArea() {
-	jQuery('#coord_picker').remove();
-	var mapOverlay = TWMap;
-	mapOverlay.mapHandler.spawnSector = mapOverlay.mapHandler._spawnSector;
-	mapOverlay.map._handleClick = mapOverlay.map._DShandleClick;
-	mapOverlay.reload();
+    jQuery('#coord_picker').remove();
+    var mapOverlay = TWMap;
+    mapOverlay.mapHandler.spawnSector = mapOverlay.mapHandler._spawnSector;
+    mapOverlay.map._handleClick = mapOverlay.map._DShandleClick;
+    mapOverlay.reload();
 }
 
 function resetCoordArea() {
-	$('.coordArea').val('')
-	UI.SuccessMessage(('Auswahl gelöscht!'), 4000);
+    $('.coordArea').val('')
+    UI.SuccessMessage(('Auswahl gelöscht!'), 4000);
 }
 
 function copyCoordArea() {
-	const coords = jQuery('#villageList').val().trim();
-	if (coords.length !== 0) {
-		jQuery('#villageList').select();
-		document.execCommand('copy');
-		UI.SuccessMessage(('Kopiert!'), 4000);
-	} else {
-		UI.ErrorMessage(('Nichts zum kopieren gefunden! :-('), 4000);
-	}
+    const coords = jQuery('#villageList').val().trim();
+    if (coords.length !== 0) {
+        jQuery('#villageList').select();
+        document.execCommand('copy');
+        UI.SuccessMessage(('Kopiert!'), 4000);
+    } else {
+        UI.ErrorMessage(('Nichts zum kopieren gefunden! :-('), 4000);
+    }
 }
 $(document).ready(function() {
-	if (jQuery('#coord_picker').length < 1) {
-		jQuery('body').append(srcHTML);
-		jQuery('#coord_picker').draggable();
-	} else {
-		UI.ErrorMessage(('Script läuft bereits!'));
-	}
+    if (jQuery('#coord_picker').length < 1) {
+        jQuery('body').append(srcHTML);
+        jQuery('#coord_picker').draggable();
+    } else {
+        UI.ErrorMessage(('Script läuft bereits!'));
+    }
     if ($('#' + outputID).length <= 0) {
         if (window.game_data.screen == 'map') {
             var srcHTML = '<div id="coord_picker" class="popup_style ui-draggable" style="width: 400px; position: fixed;display:block; top: 90px; width: auto; height: auto; background-color:#CEBC98; background-image: url(../graphic/index/main_bg.jpg); border:2px solid; visibility: visible"><div class="popup_menu ui-draggable-handle">Koordinaten-Picker<a id="closelink_village_notes_popup" style="right: 0; top: 0;" onClick="closeCoordArea();" href="#">X</a></div><div class="popup_content"><center><span style="color:black;align:center;"><b>Wählen Sie Dörfer aus der Karte aus.</b></span><br/><span style="color:black;align:center;"><i>(auf die Dörfer klicken)</span><br/><br/><center><input type="checkbox" id="cbBBEncode" onClick="isEncoded=this.checked;fnRefresh();"' + (isEncoded ? 'checked' : '') + '/>BB-Code?<br/><textarea class="coordArea" id="' + outputID + '" cols="40" rows="10" width:97%; value="" onFocus="this.select();"></textarea><br/><input type="button" class="btn btn-confirm-no" value="Auswahl löschen" onClick="resetCoordArea();"><input type="button" class="btn btn-confirm-yes" value="Auswahl Kopieren" onClick="copyCoordArea();"><br/></div></div>';
@@ -73,12 +73,12 @@ $(document).ready(function() {
                 return false;
             };
         } else {
-			UI.ErrorMessage(
-			`${('Dieses Script muss auf der Karte ausgeführt werden! <a href="/game.php?screen=map" class="btn">zur Karte</a>')}`,
-			4000
-			);
+            UI.ErrorMessage(
+                `${('Dieses Script muss auf der Karte ausgeführt werden! <a href="/game.php?screen=map" class="btn">zur Karte</a>')}`,
+                4000
+            );
         }
     }
-	mapOverlay.mapHandler._spawnSector = mapOverlay.mapHandler.spawnSector;
-	mapOverlay.map._DShandleClick = mapOverlay.map._handleClick;
+    mapOverlay.mapHandler._spawnSector = mapOverlay.mapHandler.spawnSector;
+    mapOverlay.map._DShandleClick = mapOverlay.map._handleClick;
 });
